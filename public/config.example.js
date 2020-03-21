f119ea71b53fdc41d6905520d5a964947a97afaa
Basil Zhang <zhyuz@outlook.com>
2020-03-22 00:08:34 +0800

    feat: global config file

---
+++ public/config.example.js
@@ -0,0 +1,12 @@
+window.config  = {
+  title: "Blog",
+  user: 'you',
+  repository: 'repository',
+  authors: 'you',
+  perPage: 5,
+  organization: false,
+  order: 'CREATED_AT',
+
+  host: 'host', // blog url hostname
+  hash: 'required', 
+}
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="react-scripts" />
+
+declare global {
+  interface Window {
+    config: {
+      title: string;
+      user: string;
+      repository: string;
+      authors: string;
+      perPage: number;
+      organization: boolean;
+      order: string;
+
+      host: string;
+      hash: string;
+    };
+  }
+}
